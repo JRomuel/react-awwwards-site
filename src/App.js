63466c015b95e332140f432631e73b14faa2e3d2
Romuel Webdev <johnromuelmendoza@yahoo.com>
2022-08-17 17:27:53 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,79 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import gsap from "gsap";
+
+import Header from "./components/header";
 import "./styles/App.scss";
 
+// components
+
+import CaseStudies from "./pages/caseStudies";
+import Approach from "./pages/approach";
+import Services from "./pages/services";
+import About from "./pages/about";
+import Home from "./pages/home";
+import Navigation from "./components/navigation";
+
+const routes = [
+  { path: '/', name:'Home', Component: Home},
+  { path: '/case-studies', name:'Case Studies', Component: CaseStudies},
+  { path: '/approach', name:'Approach', Component: Approach},
+  { path: '/services', name:'Services', Component: Services},
+  { path: '/about-us', name:'About Us', Component: About},
+  
+];
+
+function debounce(fn, ms){
+  let timer;
+  return () => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      timer = null;
+      fn.apply(this, arguments);
+    }, ms)
+  }
+}
+
 function App() {
+
+    // prevent flashing
+    gsap.to("body", 0, { css: { visibility: 'visible'} });
+
+  const [dimension, setDimension] = useState({
+    height: window.innerHeight,
+    width: window.innerWidth,
+  })
+  useEffect(() => {
+    let vh = dimension.height * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+    const debouncedHandleResize = debounce(function handleResize(){
+      setDimension({
+        height: window.innerHeight,
+        width: window.innerWidth
+      })
+    }, 1000);
+
+    window.addEventListener("resize", debouncedHandleResize);
+
+    return () => {
+      window.removeEventListener("resize", debouncedHandleResize);
+    };
+  });
+
   return (
-    <div className='App'>
-      <h2>Hello this is your starter files </h2>
-    </div>
+    <>
+      <Header dimension={dimension}/>
+      <div className="App">
+        <Routes>
+          {routes.map(({path, Component}) => (
+              <Route key={path} path={path} element={<Component/>}></Route>
+            ))}
+        </Routes>
+  
+      </div>
+      <Navigation/>
+    </>
   );
 }
 
---
+++ src/components/header.js
@@ -0,0 +1,144 @@
+import React, { useState, useEffect } from 'react';
+import { NavLink, useLocation  } from "react-router-dom";
+import { ReactComponent as UpArrow } from '../assets/up-arrow-circle.svg'
+
+import gsap from "gsap";
+
+let tl = gsap.timeline();
+
+const Header = ({ dimension }) => {
+  const [menuState, setMenuState] = useState({
+    menuOpened: false
+  });
+
+  const location = useLocation();
+
+  useEffect(() => {
+    setMenuState({menuOpened: false})
+  }, [location]);
+
+  useEffect(() => {
+
+    if(menuState.menuOpened === true){
+        tl.to('body', { duration: 0.01, css: { overflow: "hidden"} })
+        .to('.App',{
+            duration: 1,
+            y: dimension.width <= 500 ? '100vh' : '70vh'  ,
+            ease: "expo.inOut"
+        })
+        .to('.humburger-menu span', {
+            duration: 0.6,
+            delay: -1,
+            scaleX:0,
+            transformOrigin: '50% 0%',
+            ease: "expo.inOut"
+
+        }).to('#Path_1', {
+            duration: 0.4,
+            delay: -0.6,
+            css: {
+                strokeDashoffset: 10,
+                strokeDasharray: 5
+            }
+        }).to('#Path_2', {
+            duration: 0.4,
+            delay: -0.6,
+            css: {
+                strokeDashoffset: 10,
+                strokeDasharray: 20
+            }
+        }).to('#Line_1', {
+            duration: 0.4,
+            delay: -0.6,
+            css: {
+                strokeDashoffset: 40,
+                strokeDasharray: 18
+            }
+        }).to('#circle', {
+            duration: 0.6,
+            delay: -0.8,
+            css: {
+                strokeDashoffset: 0,
+            }
+        }).to(".humburger-menu-close", {
+            duration: 0.6,
+            delay: -0.8,
+            css: {
+                display: "block",
+            }
+        });
+    }else{
+        tl.to('.App', {
+            duration: 1,
+            y: 0,
+            ease: 'expo.inOut',
+        }).to("#circle", {
+            duration: 0.6,
+            delay: -0.6,
+            css: {
+                strokeDashoffset: -193,
+                strokeDasharray: 227
+            }
+        }).to("#Path_1", {
+            duration: 0.4,
+            delay: -0.6,
+            css: {
+                strokeDashoffset: 10,
+                strokeDasharray: 10
+            }
+        }).to("#Path_2", {
+            duration: 0.4,
+            delay: -0.6,
+            css: {
+                strokeDashoffset: 10,
+                strokeDasharray: 10
+            }
+        }).to("#Line_1", {
+            duration: 0.4,
+            delay: -0.6,
+            css: {
+                strokeDashoffset: 40,
+                strokeDasharray: 40
+            }
+        }).to('.humburger-menu span', {
+            duration: 0.6,
+            delay: -0.6,
+            scaleX:1,
+            transformOrigin: '50% 0%',
+            ease: "expo.inOut"
+
+        })
+        .to('.humburger-menu-close', {
+            css: {
+                display: "none",
+            }
+        }).to("body", {
+            css: {
+                overflow: "auto"
+            }
+        });
+    }
+  }, [menuState.menuOpened]);
+  return (
+    <div className="header">
+        <div className="container">
+            <div className="row v-center space-between">
+                <div className="logo">
+                    <NavLink to="/">AGENCY.</NavLink>
+                </div>
+                <div className="nav-toggle">
+                    <div className="humburger-menu" onClick={() => setMenuState({menuOpened: true})}>
+                        <span></span>
+                        <span></span>
+                    </div>
+                    <div className="humburger-menu-close" onClick={() => setMenuState({menuOpened: false})}>
+                        <UpArrow/>
+                     </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header;
